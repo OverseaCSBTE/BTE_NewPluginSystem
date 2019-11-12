44cc6718dfe2ced28f5bd9c5a51932dbf7ba2d51
NekoMeow <nopeople012345@gmail.com>
2019-11-12 19:35:07 +0800

    Add Compiler

---
+++ Compiler/Main.cpp
@@ -0,0 +1,12 @@
+/*	Main of CompilerLibrary
+	
+	Based on Amxmodx and Pawn
+	But we Fucked Mac and Linux Support
+	So... Who Cares?
+
+	Version : 1
+	Creator : NekoMeow
+*/
+
+#include <assert.h>	// For Debug ?
+#include <Windows.h> // Anything about windows that we want at here?
---
+++ Global/Compiler.h
@@ -0,0 +1,9 @@
+#ifndef Compiler_H
+#define Compiler_H
+
+#define PublicChar '@'
+#define CtrlChar '^'
+#define TermPreproc  '\x7f'
+#define MaxStack 4096
+
+#endif
